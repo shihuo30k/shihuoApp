676fd5bc481a0ee3c8e8a3ad471bb8e4f6192dcb
liangquan <78970494@qq.com>
2020-09-21 22:06:54 +0800

    项目启动

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,14 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    hello
   </div>
 </template>
+
+<script>
+export default {
+  
+}
+</script>
 
 <style lang="stylus">
 #app
